c9ee8dabcf1a4c22cccfdb1346db22bc7d325525
Christoph Baier <chrisby@crispy-coding.org>
2025-06-05 14:50:03 +0200

    refactoring

--- src/backend/main.go
+++ src/backend/main.go
@@ -9,6 +9,7 @@ import (
 	"ocelot/store/users"
 	"ocelot/store/versions"
 	"os"
+	"os/exec"
 	"strings"
 	"time"
 )
@@ -18,6 +19,10 @@ func init() {
 }
 
 func main() {
+	cmd := exec.Command("docker", "compose", "version")
+	if err := cmd.Run(); err != nil {
+		tools.Logger.Fatal("docker compose is not installed or not accessible in PATH. Tool is required for docker-compose.yml validation.")
+	}
 	if os.Getenv("USE_MOCK_EMAIL_CLIENT") == "true" {
 		tools.Logger.Warn("using mock email client, should only be used for testing")
 		tools.UseMailMockClient = true

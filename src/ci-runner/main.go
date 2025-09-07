c0eda2de2850ce99056d0d3aae7547fa88bc6816
Christoph Baier <chrisby@crispy-coding.org>
2025-09-07 10:09:26 +0200

    refactoring

--- src/ci-runner/main.go
+++ src/ci-runner/main.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
-	"strings"
 
 	u "github.com/ocelot-cloud/shared/utils"
 	"github.com/ocelot-cloud/task-runner"
@@ -35,17 +34,10 @@ func getAbsoluteParentDir() string {
 
 func main() {
 	tr.EnableAbortForKeystrokeControlPlusC()
-	tr.Config.CleanupFunc = func() {
-		var potentiallyExistingProcesses = []string{
-			"vue-tr-service",
-			"vue-service",
-			"vite",
-		}
-		tr.KillProcesses(potentiallyExistingProcesses)
-	}
-	defer tr.Cleanup()
 
-	// TODO !! get rid of those env variables; only use "PROFILE=TEST"
+	// TODO !! get rid of those env variables; only use "PROFILE=TEST" when doing component test
+	// TODO !! also re-add production test
+	// TODO !! when deploying to hetzner server, I should have a
 	tr.Config.DefaultEnvironmentVariables = []string{"USE_MOCK_EMAIL_CLIENT=true", "RUN_NATIVELY=true", "LOG_LEVEL=DEBUG"}
 
 	rootCmd := &cobra.Command{
@@ -53,6 +45,7 @@ func main() {
 		Short: "ci-runner is a service that runs CI jobs",
 	}
 
+	testCmd.AddCommand(testUnitsCmd, testComponentCmd, testAllCmd)
 	buildCmd.AddCommand(buildBackendCmd)
 	rootCmd.AddCommand(testCmd, updateCmd, deployCmd, analyzeCmd, buildCmd)
 	rootCmd.CompletionOptions.DisableDefaultCmd = true
@@ -81,12 +74,14 @@ var deployCmd = &cobra.Command{
 	Use:   "deploy",
 	Short: "Deploy current version to server",
 	Run: func(cmd *cobra.Command, args []string) {
+		// TODO !! rather upload to dockerhub and pull from there to server
 		var prompt = "Are you sure you want to replace the current production version of the App Store?"
 		tr.PromptForContinuation(prompt)
 		tr.ExecuteInDir(backendDir, "go build -a -installsuffix cgo", "CGO_ENABLED=0", "GOOS=linux", "GOARCH=amd64")
 		executeOnServer("docker rm -f store")
 		rsyncCmd := fmt.Sprintf("rsync -avz --delete docker/Dockerfile docker/docker-compose.yml assets store dist %s:", sshHost)
 		tr.ExecuteInDir(backendDir, rsyncCmd)
+		// TODO !! why two times docker compose up? Can be removed?
 		executeOnServer("docker compose up -d")
 		executeOnServer("docker compose up -d --build --force-recreate --remove-orphans store")
 	},
@@ -98,35 +93,35 @@ func executeOnServer(command string) {
 }
 
 var testCmd = &cobra.Command{
-	Use:   "test [" + strings.Join(getKeys(hubTestTypes), "/") + "]",
-	Short: "Run tests",
-	Args:  cobra.ExactArgs(1),
+	Use:   "test",
+	Short: "run tests",
 	Run: func(cmd *cobra.Command, args []string) {
-		inputTestType := args[0]
-		if _, exists := hubTestTypes[inputTestType]; !exists {
-			tr.Log.Error("\nerror: unknown hub test type: %s\n", inputTestType)
-			tr.Log.Error("valid args: %s\n", strings.Join(getKeys(hubTestTypes), ", "))
-			os.Exit(1)
-		} else {
-			hubTestTypes[inputTestType]()
-		}
-		tr.Log.Info(("\nSuccess! Hub tests passed.\n"))
+		cmd.Help()
 	},
 }
 
-// TODO !! rather make commands out of this
-var hubTestTypes = map[string]func(){
-	"units":     func() { TestUnits() },
-	"component": func() { TestComponent() },
-	"all":       func() { TestAll() },
+var testUnitsCmd = &cobra.Command{
+	Use:   "units",
+	Short: "run unit tests",
+	Run: func(cmd *cobra.Command, args []string) {
+		TestUnits()
+	},
 }
 
-func getKeys(m map[string]func()) []string {
-	keys := make([]string, 0, len(m))
-	for k := range m {
-		keys = append(keys, k)
-	}
-	return keys
+var testComponentCmd = &cobra.Command{
+	Use:   "component",
+	Short: "run component tests",
+	Run: func(cmd *cobra.Command, args []string) {
+		TestComponent()
+	},
+}
+
+var testAllCmd = &cobra.Command{
+	Use:   "all",
+	Short: "run all tests",
+	Run: func(cmd *cobra.Command, args []string) {
+		TestAll()
+	},
 }
 
 var analyzeCmd = &cobra.Command{
@@ -147,7 +142,7 @@ var buildCmd = &cobra.Command{
 
 var buildBackendCmd = &cobra.Command{
 	Use:   "backend",
-	Short: "Builds the backend",
+	Short: "builds the backend including all production code and test code to find compilation errors",
 	Run: func(cmd *cobra.Command, args []string) {
 		u.BuildWholeGoProject(tr, backendDir)
 	},

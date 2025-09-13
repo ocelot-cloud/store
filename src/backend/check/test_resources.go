b16f88fffb91148f05e637c54e5d75fcc912f7f2
Christoph Baier <chrisby@crispy-coding.org>
2025-09-13 13:03:50 +0200

    refactoring

--- src/backend/check/test_resources.go
+++ src/backend/check/test_resources.go
@@ -3,10 +3,8 @@ package check
 import (
 	"ocelot/store/tools"
 	"ocelot/store/users"
-	"os"
 	"testing"
 
-	"github.com/ocelot-cloud/deepstack"
 	"github.com/ocelot-cloud/shared/assert"
 	"github.com/ocelot-cloud/shared/store"
 	u "github.com/ocelot-cloud/shared/utils"
@@ -18,14 +16,12 @@ var SampleVersionFileContent = GetValidVersionBytesOfSampleMaintainerApp()
 func GetValidVersionBytesOfSampleMaintainerApp() []byte {
 	assetsDir, err := u.FindDir("assets")
 	if err != nil {
-		u.Logger.Error("Failed to find assets directory", deepstack.ErrorField, err)
-		// TODO !! return error
+		panic("Failed to find assets directory")
 	}
 	sampleAppDir := assetsDir + "/sample-app"
 	versionBytes, err := validation.ZipDirectory(sampleAppDir)
 	if err != nil {
-		u.Logger.Error("Failed to read sample version file", deepstack.ErrorField, err)
-		os.Exit(1)
+		panic("Failed to read sample version file")
 	}
 	return versionBytes
 }
--- src/backend/tools/base_repo.go
+++ src/backend/tools/base_repo.go
@@ -2,10 +2,8 @@ package tools
 
 import (
 	"database/sql"
-	"os"
 
 	_ "github.com/jackc/pgx/v5/stdlib"
-	"github.com/ocelot-cloud/deepstack"
 	u "github.com/ocelot-cloud/shared/utils"
 )
 
@@ -21,24 +19,17 @@ func (d *DatabaseProviderImpl) GetDb() *sql.DB {
 func (d *DatabaseProviderImpl) InitializeDatabase() error {
 	var err error
 	var host, customPostgresPort string
-	// TODO maybe better introduce profiles? -> so acceptance testing should be run against the app store container I guess?
 	host = "ocelotcloud_store_postgres"
 	customPostgresPort = "5432"
 
-	// TODO !! get rid of exit() and return error to top module instead
-
 	d.Db, err = u.WaitForPostgresDb(host, customPostgresPort)
 	if err != nil {
-		u.Logger.Error("Failed to create database client", deepstack.ErrorField, err)
-		os.Exit(1)
+		return err
 	}
-	u.Logger.Info("Database client created successfully")
 
 	err = u.RunMigrations(d.PathProvider.GetMigrationsDir(), host, customPostgresPort)
 	if err != nil {
-		u.Logger.Error("Failed to run migrations", deepstack.ErrorField, err)
-		os.Exit(1)
+		return err
 	}
-
 	return nil
 }
